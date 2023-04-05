faaf9e2320229dfda4638d0a2d45a97622ef63d0
Kazel Deligero <kazel_deligero@yahoo.com>
2023-04-06 01:03:24 +0800

    partial portfolio_crud

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_093153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "portfolios", force: :cascade do |t|
+    t.string "symbol"
+    t.string "company_name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
