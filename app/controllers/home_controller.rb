87807789538bcf2b1f79e94088185f2ccd6436ec
Kazel Deligero <kazel_deligero@yahoo.com>
2023-04-12 22:39:34 +0800

    feat: add iex chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,16 @@
 class HomeController < ApplicationController
  before_action :get_user
-
  def index
-    @client = IEX::Api::Client.new(
-        publishable_token: 'pk_06f0670b09884fe5aa66d394e4263f00',
-        endpoint: 'https://cloud.iexapis.com/v1'
-      )
-    @all = @client.stock_market_list(:mostactive)
+  $stocks = show_stocks
+ end
+
+ def show_stocks
+  response = Faraday.get('https://api.iex.cloud/v1/data/CORE/REF_DATA?token=pk_06f0670b09884fe5aa66d394e4263f00')
+  JSON.parse(response.body)
  end
 
  private
+
  def get_user
      @user = User.find_by_email(cookies.encrypted[:user_id])
     
